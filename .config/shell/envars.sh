699d316d7210ec7c39d363fba36ab28d9af6d5d5
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-02 09:43:50 +0530

    Add shell config files

---
+++ .config/shell/envars.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Environment variables
+export EDITOR=nvim
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_COMPLETION_TRIGGER=','
+export GREP_COLOR="1;32"
+export ZSH_PLUGINS_ALIAS_TIPS_TEXT="Alias: "
+export ZSH_TMUX_AUTOSTART='false'
+export ZSH_TMUX_AUTOSTART_ONCE='false'
+export ZSH_TMUX_AUTOCONNECT='false'
+export DISABLE_AUTO_TITLE='true'
+
+export GOPATH=$HOME/.go
+export GOBIN=$GOPATH/bin
+
+export PATH=$HOME/Applications/bin:$PATH
+export PATH=$GOBIN:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/.gem/ruby/3.0.0/bin:$PATH
+export PATH=$HOME/.npm/bin:$PATH
+#export PATH="$HOME/.pyenv/bin:$PATH"
+#eval "$(pyenv init --path)"
+
+export CHROME_EXECUTABLE=/usr/bin/google-chrome-stable
+
+#eval "$(dircolors ~/.dircolors)";
+# eval "$(thefuck --alias)"
+
+# Node Version Manager
+[ -f /usr/share/nvm/init-nvm.sh ] && source /usr/share/nvm/init-nvm.sh
+
+# broot file explorer
+[ -f ~/.config/broot/launcher/bash/br ] && source ~/.config/broot/launcher/bash/br
+
+# Homebrew
+[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+[ -f "${HOMEBREW_PREFIX}/etc/bash_completion.d/brew" ] && source "${HOMEBREW_PREFIX}/etc/bash_completion.d/brew"
+
---
+++ .config/shell/functions.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+# Functions
+function color() {
+    local fgc bgc vals seq0
+
+    printf "Color escapes are %s\n" '\e[${value};...;${value}m'
+    printf "Values 30..37 are \e[33mforeground colors\e[m\n"
+    printf "Values 40..47 are \e[43mbackground colors\e[m\n"
+    printf "Value  1 gives a  \e[1mbold-faced look\e[m\n\n"
+
+    # foreground colors
+    for fgc in {30..37}; do
+        # background colors
+        for bgc in {40..47}; do
+            fgc=${fgc#37} # white
+            bgc=${bgc#40} # black
+
+            vals="${fgc:+$fgc;}${bgc}"
+            vals=${vals%%;}
+
+            seq0="${vals:+\e[${vals}m}"
+            printf "  %-9s" "${seq0:-(default)}"
+            printf " ${seq0}TEXT\e[m"
+            printf " \e[${vals:+${vals+$vals;}}1mBOLD\e[m"
+        done
+        echo
+        echo
+    done
+}
+
+# cd into dir and list contents
+function lc() {
+    cd "$1" && la "$2"
+}
+
+# Make directory and cd into it
+function mcd() {
+    mkdir -p -- "$1" && cd -P -- "$1"
+}
+
+function tmux-clean() {
+    tmux list-sessions | grep -E -v '\(attached\)$' | while IFS='\n' read line; do
+        tmux kill-session -t "${line%%:*}"
+    done
+}
+
+# Execute command in directory
+function xin() {
+    (cd "${1}" && shift && ${@})
+}
+
+function man() {
+    env \
+        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[1;36m") \
+        LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_se=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+        LESS_TERMCAP_ue=$(printf "\e[0m") \
+        LESS_TERMCAP_us=$(printf "\e[1;32m") \
+        PAGER="${commands[less]:-$PAGER}" \
+        _NROFF_U=1 \
+        PATH="$HOME/bin:$PATH" \
+        man "$@"
+}
+
+# markdown link check
+function mlc () {
+    find $1 -name \*.md -exec markdown-link-check -p {} \;
+}
+
+# ex - archive extractor
+function ex ()
+{
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1   ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+lg() {
+    export LAZYGIT_NEW_DIR_FILE=~/.config/lazygit/.tmp
+    lazygit "$@"
+
+    if [ -f $LAZYGIT_NEW_DIR_FILE ]; then
+            cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
+            rm -f $LAZYGIT_NEW_DIR_FILE > /dev/null
+    fi
+}
+
+# Runs when tab is pressed after ,
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf "$@" --preview 'exa -TFl --group-directories-first --icons --git -L 2 --no-user {}' ;;
+    nvim)         fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' ;;
+    vim)          fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' ;;
+    *)            fzf "$@" ;;
+  esac
+}
+
+# Only show files which have $1 present in contents
+faz(){
+  local line
+  line=`ag --nocolor "$1" | fzf` \
+    && $EDITOR $(cut -d':' -f1 <<< "$line") +$(cut -d':' -f2 <<< "$line")
+}
