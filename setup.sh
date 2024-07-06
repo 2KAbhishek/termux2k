12da095c44f99da3115d7e06c6a6f445daa20b6a
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-07-06 10:46:59 +0530

    feat: update setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 setup_termux2k() {
-    cd "$PWD/dots2k" && ./setup.sh && cd .. || exit 1
+    cd "$PWD/dots2k" && ./setup.sh "$@" && cd .. || exit 1
 }
 
 main() {
     if [ -f "$PWD/dots2k/setup.sh" ]; then
-        setup_termux2k
+        setup_termux2k "$@"
     else
         git submodule update --init --recursive --remote && setup_termux2k
     fi
