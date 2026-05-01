d1974b5c5bbc9d70b0f95f4e2120b7f051621385
Abhishek Keshri <iam2kabhishek@gmail.com>
2026-05-01 11:03:53 +0530

    feat: add termux config linking

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+TERMUX2K_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
 setup_termux2k() {
     cd "$PWD/dots2k" && ./setup.sh "$@" && cd .. || exit 1
 }
 
 main() {
+    cp -rv "$TERMUX2K_DIR/config/.termux" ~/
+    ln -sfnv "$TERMUX2K_DIR/config/bin" ~/
     if [ -f "$PWD/dots2k/setup.sh" ]; then
         setup_termux2k "$@"
     else
